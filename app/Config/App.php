7a165023d8abfd17092fac164f663f2f82d91679
YudhiFitri <fitriwah84@gmail.com>
2022-07-11 13:37:25 +0700

    commit at 11072022 1337

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    // public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://192.168.10.3/gigams/';
 
     /**
      * --------------------------------------------------------------------------
@@ -55,7 +56,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $uriProtocol = 'REQUEST_URI';
+    // public $uriProtocol = 'REQUEST_URI';
+    public $uriProtocol = 'PATH_INFO';
 
     /**
      * --------------------------------------------------------------------------
